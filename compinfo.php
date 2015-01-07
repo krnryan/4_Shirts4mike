537cc00cf0a6af1d17edc447f518e561435a580f
Ryan Choi <ryan.mingyu.choi@gmail.com>
2015-01-06 23:41:47 -0800

    first commit

---
+++ compinfo.php
@@ -0,0 +1,13 @@
+<?php
+require_once('inc/config.php');
+$pageTitle = "About Mike's Shirts";
+$section = "compinfo";
+include('inc/header.php'); ?>
+
+    <div class="section page">
+
+        <h1>About us</h1>
+        
+    </div>
+
+<?php include('inc/footer.php'); ?>
